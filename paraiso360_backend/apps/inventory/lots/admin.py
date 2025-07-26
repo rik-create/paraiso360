98cf9328ecf1756e23e488338e13d3bc8018cb6f
rik-create <154645824+rik-create@users.noreply.github.com>
2025-07-26 14:21:08 +0800

    Register Lot model with custom admin interface for improved display

--- paraiso360_backend/apps/inventory/lots/admin.py
+++ paraiso360_backend/apps/inventory/lots/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Lot
 
-admin.site.register(Lot)
+
+@admin.register(Lot)
+class LotAdmin(admin.ModelAdmin):
+    list_display = ['id', 'lot_number', 'block',
+                    'section', 'status', 'client', 'lot_type']
